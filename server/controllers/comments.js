c9d7cd1187d950f11f613a15c12344dc8fc1163c
rapraquion <rapraquion@gmail.com>
2020-01-06 22:39:45 +0800

    added posts, comments and profiles table

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,16 +1,35 @@
-function newPost(req, res) {
+function newComment(req, res) {
     const db = req.app.get('db');
 
+    db.comments
+        .save({
+            userId,
+            postId,
+            comment
+        })
+        .then(com => res.status(201).json(com))
+        .catch(e => {
+            console.error(e);
+            res.status(500).end();
+        })
 }
 
-function getUserSinglePost(req, res) {
+function editComment(req, res) {
     const db = req.app.get('db');
 
+    const { comment } = req.body
+
+    db.comments
+        .update({ 
+            id: req.params.id 
+        },{
+            comment: comment
+        })
+        .then(p => res.status(201).send(comment))
+        .catch(e => {
+            console.error(e);
+            res.status(500).end();
+        })
 }
 
-function getAllPosts(req, res) {
-    const db = req.app.get('db');
-
-}
-
-module.exports = { newPost, getUserSinglePost, getAllPosts }
+module.exports = { newComment, editComment }
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,15 +1,60 @@
-function newComment(req, res) {
+function newPost(req, res) {
+    const db = req.app.get('db');
+
+    const { userId, content } = req.body
+
+    db.posts
+        .save({
+            userId,
+            content
+        })
+        .then(p => res.status(201).json(post))
+        .catch(e => {
+            console.error(e);
+            res.status(500).end();
+        })
+}
+
+function getUserSinglePost(req, res) {
     const db = req.app.get('db');
 
 }
 
-function editComment(req, res) {
+function getAllPosts(req, res) {
     const db = req.app.get('db');
 
+    db.posts
+        .find(p => {
+            db.comments
+                .find({postId: req.params.id})
+                .then(comment => res.status(200).json({post, comment}))
+                .catch(e => {
+                    console.error(e);
+                    res.status(500).end();
+                })
+        })
+        .catch(e => {
+            console.error(e);
+            res.status(500).end();
+        })
 }
 
 function updatePost(req, res) {
     const db = req.app.get('db');
+
+    const { content } = req.body
+
+    db.posts
+        .update({
+            id: req.params.id
+        },{
+            content: content
+        })
+        .then(p => res.status(201).send(post))
+        .catch(e => {
+            console.error(e);
+            res.status(500).end();
+        })
 }
 
-module.exports = { newComment, editComment, updatePost }
+module.exports = { newPost, getUserSinglePost, getAllPosts, updatePost }
